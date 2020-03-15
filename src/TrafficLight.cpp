a846a96a7a411e89f1026c1a27b2a3ea9ee234e1
Robotawi <mohamed.s.raessa@ieee.org>
2020-03-15 13:50:56 +0900

    Added TrafficLight class implementation

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -4,13 +4,19 @@
 
 /* Implementation of class "MessageQueue" */
 
-/* 
+
 template <typename T>
 T MessageQueue<T>::receive()
 {
     // FP.5a : The method receive should use std::unique_lock<std::mutex> and _condition.wait() 
     // to wait for and receive new messages and pull them from the queue using move semantics. 
-    // The received object should then be returned by the receive function. 
+    // The received object should then be returned by the receive function.
+    std::unique_lock<std::mutex> _ulck(_mtx);
+    //TODO: recheck here 1
+    _cond.wait(_ulck,[this](std::deque<T> _queue){return !_queue.empty();});
+    T msg = std::move(_queue.back());
+    _queue.pop_back();
+    return msg;
 }
 
 template <typename T>
@@ -18,12 +24,15 @@ void MessageQueue<T>::send(T &&msg)
 {
     // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex> 
     // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
+    std::lock_guard<std::mutex> lck(_mtx);
+    //TODO: recheck here 2
+    _queue.emplace_back(std::move(msg));
+    _cond.notify_one();
 }
-*/
+
 
 /* Implementation of class "TrafficLight" */
 
-/* 
 TrafficLight::TrafficLight()
 {
     _currentPhase = TrafficLightPhase::red;
@@ -52,7 +61,22 @@ void TrafficLight::cycleThroughPhases()
     // FP.2a : Implement the function with an infinite loop that measures the time between two loop cycles 
     // and toggles the current phase of the traffic light between red and green and sends an update method 
     // to the message queue using move semantics. The cycle duration should be a random value between 4 and 6 seconds. 
-    // Also, the while-loop should use std::this_thread::sleep_for to wait 1ms between two cycles. 
+    // Also, the while-loop should use std::this_thread::sleep_for to wait 1ms between two cycles.
+    std::random_device rdev;
+    while(true){
+        std::mt19937 genseed(rdev());
+        std::uniform_int_distribution<> dis_range(4, 6);
+        auto randy = dis_range(genseed) + 1000;
+        std::this_thread::sleep_for(std::chrono::milliseconds(randy));
+
+        if (_currentPhase == TrafficLightPhase::green){
+            _currentPhase = TrafficLightPhase::red;
+        }
+        else{
+            _currentPhase = TrafficLightPhase::green;
+        }
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    }
 }
 
-*/
